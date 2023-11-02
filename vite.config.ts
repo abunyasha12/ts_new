0c665f004a2cbf19b2c990d77ab826c29351314a
abunyasha12 <43925928+abunyasha12@users.noreply.github.com>
2023-11-02 07:16:07 +0500

    quickfix

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/ts_new/'
+    : '/',
   plugins: [
     vue({
       template: { transformAssetUrls }
